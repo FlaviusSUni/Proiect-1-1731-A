47e4668150aea2b667bc1a7ef0b62f102ea34878
FlaviusSUni <stasac.flaviusgabriel@student.uoradea.ro>
2025-10-30 19:13:51 +0200

    server routes controller

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import productRoutes from './routes/productRoutes.js';
+import { sql } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use('/api/products', productRoutes);
+
+async function initDB() {
+    try {
+        await sql`
+        CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            image VARCHAR(255) NOT NULL,
+            price DECIMAL(10, 2) NOT NULL,
+            created_at TIMESTAMP DEFAULT NOW()
+        );
+        `;
+        console.log('Database connected and ensured products table exists.');
+        
+    } catch (error) {
+        console.log('Database connection failed:', error);
+        
+    }
+}
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
