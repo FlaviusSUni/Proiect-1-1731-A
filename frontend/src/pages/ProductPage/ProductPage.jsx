ab2bd731623caf581257cfbda2841206b5b08358
FlaviusSUni <stasac.flaviusgabriel@student.uoradea.ro>
2025-11-13 20:14:40 +0200

    fetch data

---
+++ frontend/src/pages/ProductPage/ProductPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductPage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-green-600">Welcome to the Product Page!</h1>
+    </div>
+  )
+}
+
+export default ProductPage
